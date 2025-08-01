243fb8f4295670d8c3c31413f6d75f61d14171f9
Joshua Carroll <carrolljosh1976@gmail.com>
2025-07-31 23:18:40 -0400

    created Header component to build out app Header

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://discrete-quagga-59.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
