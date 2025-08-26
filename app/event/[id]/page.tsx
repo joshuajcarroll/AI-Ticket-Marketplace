9ebe6e7cbf2f3f7d3980edf9c8c97fa9bd76b735
user.email <zettafan40@yahoo.com>
2025-08-26 05:58:46 -0400

    added Image component for event image to EventPage, started to add content for event details

--- app/event/[id]/page.tsx
+++ app/event/[id]/page.tsx
@@ -4,6 +4,7 @@ import { Id } from "@/convex/_generated/dataModel";
 import { useStorageUrl } from "@/lib/utils";
 import { useUser } from "@clerk/nextjs";
 import { useQuery } from "convex/react";
+import Image from "next/image";
 import { useParams } from "next/navigation";
 
 function EventPage() {
@@ -22,7 +23,26 @@ function EventPage() {
     <div className="min-h-screen bg-gray-50">
         {/*Event Details Section */}
         <div className="max-w-7xl mx-auto p-4 sm:px-6 lg:px-8 py-12">
-            <div className="bg-white rounded-xl shadow-sm overflow-hidden"></div>
+            <div className="bg-white rounded-xl shadow-sm overflow-hidden">
+                {/* Event Image */}
+                {imageUrl && (
+                    <div className="aspect-[21/9] relative w-full">
+                        <Image
+                            src={imageUrl}
+                            alt={event.name}
+                            fill
+                            className="object-cover"
+                            priority
+                        />
+                    </div>
+                )}
+                {/*Event Details in depth */}
+                <div className="p-8">
+                    <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
     
