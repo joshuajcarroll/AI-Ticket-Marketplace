bd5603aab28d443b7bfd44c58906f95e9a3d5685
user.email <zettafan40@yahoo.com>
2025-08-28 15:39:39 -0400

    added more details for event on EventPage

--- app/event/[id]/page.tsx
+++ app/event/[id]/page.tsx
@@ -4,6 +4,7 @@ import { Id } from "@/convex/_generated/dataModel";
 import { useStorageUrl } from "@/lib/utils";
 import { useUser } from "@clerk/nextjs";
 import { useQuery } from "convex/react";
+import { CalendarDays } from "lucide-react";
 import Image from "next/image";
 import { useParams } from "next/navigation";
 
@@ -43,6 +44,17 @@ function EventPage() {
                             <div>
                                 <h1 className="text-4xl font-bold text-gray-900">{event.name}</h1>
                                 <p className="text-lg text-gray-600">{event.description}</p>
+                                <div className="grid grid-cols-2 gap-6">
+                                    <div className="bg-gray-50 p-4 rounded-lg border border-gray-100">
+                                        <div className="flex items-center text-gray-600 mb-1">
+                                            <CalendarDays className="w-5 h-5 mr-2 text-blue-600" />
+                                            <span className="text-sm font-medium">Date</span>
+                                        </div>
+                                        <p className="text-gray-900">
+                                            new Date({event.eventDate}).toLocaleDateString();
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
